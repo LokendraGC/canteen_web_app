253a42f0f723ff3a3b33a7d5b5ae951b38ede89a
LokendraGC <gclokendra10@gmail.com>
2023-11-08 13:21:48 +0545

    responsive navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,19 @@
+"use client"
 import React from "react";
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
+import {RiMenu2Fill} from 'react-icons/ri'
+import {MdRestaurantMenu} from 'react-icons/md'
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -14,13 +25,13 @@ const Navbar = () => {
           <div className={styles.text}> 9848114263</div>
         </div>
       </div>
+
+      
       <div className={styles.item}>
-        <ul className={styles.list}>
+        <ul className={menuOpen ? styles.listMobile : styles.list}>
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          {/* <Image 
-          src="/Img/logo_thumb.png" alt="" width={20} height={20} /> */}
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
@@ -30,6 +41,18 @@ const Navbar = () => {
           <Image src="/Img/cart.png" alt="" width={30} height={30} />
           <div className={styles.counter}>2</div>
         </div>
+          <RiMenu2Fill
+            size={30}
+            className={styles.menu}
+            onClick={toggleMenu}
+            style={{ display: menuOpen ? "none" : "block" }}
+          />
+          <MdRestaurantMenu
+            size={30}
+            className={styles.cross}
+            onClick={toggleMenu}
+            style={{ display: menuOpen ? "block" : "none" }}
+          />
       </div>
     </div>
   );
